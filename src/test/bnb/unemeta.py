49b8c64c9e533613dd2f477c6cf5180d2eba546a
yinyunlong <yinyunlong@xiaomi.com>
2023-09-07 16:05:27 +0800

    #unemeta 代码提交

--- src/test/bnb/unemeta.py
+++ src/test/bnb/unemeta.py
@@ -4,6 +4,7 @@ from src.task_core.tools.block_chain import Block_chain
 from src.task_core.tools.web3_wrap import Web3Wrap
 
 w = Web3Wrap.get_instance(block_chain=Block_chain.BSC_ANKR, gas_flag=False)
+
 def query_point(w, address='', network=''):
     url ='https://www.unemeta.com/api/backend/api/integral/v1/uu/info?location=0'
 
@@ -33,17 +34,17 @@ def query_point(w, address='', network=''):
     }
     rsp = w.session.request(method='get', url=url, headers=headers)
 
-    print('[query_point] status code:', rsp.status_code, ' encoding:', rsp.encoding,' content:', str(rsp.content, encoding='br'))
+    print('[query_point] status code:', rsp.status_code, ' encoding:', rsp.encoding,' content:', rsp.content)
     pass
 
 
-#query_point(w)
+
+#www.unemeta.com/zh/rewards?invitationCode=true
+#eheltl
+#
+#
 
 
-#https://www.unemeta.com/api/backend/api/user/v1/users/nonce
-#{
-#  "walletAddress": "0x1Ee62353F811fB4C78F05fF7f3bF09BE4DaEa6c8"
-#}
 
 #{"code":200,"msg":"OK","data":{"noce":"Welcome to Unemeta!\nThis request will not trigger a blockchain transaction or cost any gas fees.\n\nYour authentication status will reset after 24 hours.\n  \nWallet address:\n0x1Ee62353F811fB4C78F05fF7f3bF09BE4DaEa6c8       \nNonce:\n1693989496"}}
 
@@ -62,9 +63,28 @@ def query_point(w, address='', network=''):
 #
 #{"code":200,"msg":"OK","data":{"accessToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50S2V5IjoiIiwiY2VudHJhbEVtYWlsIjoiIiwiY2VudHJhbElkIjowLCJleHAiOjE2OTQwNzU5MDMsImlhdCI6MTY5Mzk4OTUwMywiand0VXNlcklkIjo3MDE5OSwid2FsbGV0QWRkcmVzcyI6IjB4MUVlNjIzNTNGODExZkI0Qzc4RjA1ZkY3ZjNiRjA5QkU0RGFFYTZjOCJ9.llw6fNVYzW2ugeI5JwBtFpSpPADLEgGLt7F8Ylh0vK0","accessExpire":1694162303,"refreshAfter":1694075903}}
 a1 = account_1
-content = {"code":200,"msg":"OK","data":{"noce":"Welcome to Unemeta!\nThis request will not trigger a blockchain transaction or cost any gas fees.\n\nYour authentication status will reset after 24 hours.\n  \nWallet address:\n0x1Ee62353F811fB4C78F05fF7f3bF09BE4DaEa6c8       \nNonce:\n1693989496"}}
 
-print(a1.address)
-sig = w.sign_data(a1,content['data']['noce'])
-print(11133)
-print(sig)
+
+#https://www.unemeta.com/api/backend/api/user/v1/users/nonce
+#{
+#  "walletAddress": "0x1Ee62353F811fB4C78F05fF7f3bF09BE4DaEa6c8"
+#}
+def nonce(w, a1):
+    url ='https://www.unemeta.com/api/backend/api/user/v1/users/nonce'
+    payload = {"walletAddress": a1.address}
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+    rsp = w.session.request(method='post', url=url, headers=headers, data=json.dumps(payload))
+    print('status:', rsp.status_code, ' rsp:', rsp.json())
+
+
+
+nonce(w, a1)
+
+
+#content = {"code":200,"msg":"OK","data":{"noce":"Welcome to Unemeta!\nThis request will not trigger a blockchain transaction or cost any gas fees.\n\nYour authentication status will reset after 24 hours.\n  \nWallet address:\n0x1Ee62353F811fB4C78F05fF7f3bF09BE4DaEa6c8       \nNonce:\n1693989496"}}
+#sig = w.sign_data(a1,content['data']['noce'])
+#print(sig)
